5cfee7015b0a9fdd0dc0938927772cb33c625cf8
Richardjackson-web <richardjackson556@gmail.com>
2020-10-12 19:11:00 +0100

    Made a couple of changes

--- js/index.js
+++ js/index.js
@@ -1,10 +1,25 @@
-document.getElementById("studentForm").style.display = "none"
-document.getElementById("nonStudentForm").style.display = "none"
+document.getElementById("studentForm").style.display = "none";
+document.getElementById("nonStudentForm").style.display = "none";
 
 function studentClick1() {
-    document.getElementById("studentForm").style.display = "block"
+    document.getElementById("studentForm").style.display = "block";
+    document.getElementById("nonStudentForm").style.display ="none";
+    document.getElementById("loginForm").style.display = "none";
 }
 
 function studentClick2() {
-    document.getElementById("nonStudentForm").style.display = "block"
+    document.getElementById("nonStudentForm").style.display = "block";
+    document.getElementById("studentForm").style.display = "none";
+    document.getElementById("loginForm").style.display = "none";
 }
+
+function openForm() {
+    document.getElementById("loginForm").style.display = "block";
+    document.getElementById("studentForm").style.display = "none";
+    document.getElementById("nonStudentForm").style.display = "none";
+
+  }
+  
+  function closeForm() {
+    document.getElementById("loginForm").style.display = "none";
+  }
