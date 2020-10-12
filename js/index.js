e620bf17b4dfb5a1123cea440b4eeb2abec77a13
Richardjackson-web <richardjackson556@gmail.com>
2020-10-12 14:25:48 +0100

    Second Commit

---
+++ js/index.js
@@ -0,0 +1,10 @@
+document.getElementById("studentForm").style.display = "none"
+document.getElementById("nonStudentForm").style.display = "none"
+
+function studentClick1() {
+    document.getElementById("studentForm").style.display = "block"
+}
+
+function studentClick2() {
+    document.getElementById("nonStudentForm").style.display = "block"
+}
