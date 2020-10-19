b17da3f703cd2e7d0901698e27ea4c28a3655874
Richardjackson-web <richardjackson556@gmail.com>
2020-10-19 17:17:12 +0100

    Made changes to the nav bar and creaeted a footer

--- js/index.js
+++ js/index.js
@@ -27,3 +27,15 @@ function openForm() {
     document.getElementById("nonStudentForm").style.display = "none";
     document.getElementById("student").style.display = "none";
   }
+
+
+
+//   $(function () {
+//     $(window).on('scroll', function () {
+//         if ( $(window).scrollTop() > 10 ) {
+//             $('.navbar').addClass('active');
+//         } else {
+//             $('.navbar').removeClass('active');
+//         }
+//     });
+// });
